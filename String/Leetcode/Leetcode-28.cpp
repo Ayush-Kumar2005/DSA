190c3cde583db5e4206634a9e6cb4fc61f31efca
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-03 23:59:23 +0530

    Leetcode 28 3rd approach

--- String/Leetcode/Leetcode-28.cpp
+++ String/Leetcode/Leetcode-28.cpp
@@ -22,3 +22,33 @@
 //         return -1;
 //     }
 // };
+
+
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int n = haystack.size(), m = needle.size();
+//         if (m == 0) return 0;
+//         if (m > n) return -1;
+
+//         const int p = 31;
+//         const int mod = 1e9 + 7;
+
+//         long long needleHash = 0, hayHash = 0, power = 1;
+//         for (int i = 0; i < m; i++) {
+//             needleHash = (needleHash * p + needle[i]) % mod;
+//             hayHash = (hayHash * p + haystack[i]) % mod;
+//             if (i < m - 1) power = (power * p) % mod;
+//         }
+
+//         for (int i = 0; i <= n - m; i++) {
+//             if (hayHash == needleHash && haystack.substr(i, m) == needle)
+//                 return i;
+//             if (i < n - m) {
+//                 hayHash = (hayHash - haystack[i] * power % mod + mod) % mod;
+//                 hayHash = (hayHash * p + haystack[i + m]) % mod;
+//             }
+//         }
+//         return -1;
+//     }
+// };
