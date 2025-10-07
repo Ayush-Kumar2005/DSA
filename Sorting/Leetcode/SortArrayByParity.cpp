aae1071485bf3ed107081903717f5f8a64bc588e
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-07 22:10:53 +0530

    Sort Array by parity 2nd Approach

--- Sorting/Leetcode/SortArrayByParity.cpp
+++ Sorting/Leetcode/SortArrayByParity.cpp
@@ -14,3 +14,23 @@
 //         return arr;
 //     }
 // };
+
+
+// class Solution {
+// public:
+//     vector<int> sortArrayByParity(vector<int>& arr) {
+//         int n=arr.size();
+//         vector<int>ans;
+//         for(int i=0;i<n;i++){
+//             if(arr[i]%2==0){
+//                 ans.push_back(arr[i]);
+//             }
+//         }
+//         for(int i=0;i<n;i++){
+//             if(arr[i]%2!=0){
+//                 ans.push_back(arr[i]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
