9c1b9b195f7cd4aaca5d36dc644faf8c89e7256e
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-07 22:26:30 +0530

    Sort Array By Increasing Frequencies

---
+++ Sorting/Leetcode/SortArrayByIncreasingFrequency.cpp
@@ -0,0 +1,12 @@
+// vector<int> frequencySort(vector<int>& nums) {
+//     unordered_map<int, int> freq;
+//     for (int x : nums) freq[x]++;
+
+//     sort(nums.begin(), nums.end(), [&](int a, int b) {
+//         if (freq[a] == freq[b])
+//             return a > b; // descending by value
+//         return freq[a] < freq[b]; // ascending by frequency
+//     });
+
+//     return nums;
+// }
