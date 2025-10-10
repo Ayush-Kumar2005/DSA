8f782a8821710acf2f2267e8a78ebedb626fcaa3
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-10 10:38:04 +0530

    Count number of noed OPTIMIZATION

--- BinaryTree/Leetcode/SymmetricTree.cpp
+++ BinaryTree/Leetcode/SymmetricTree.cpp
@@ -1,15 +1,55 @@
+// class Solution {
+// public:
+//     bool helper(TreeNode* p, TreeNode* q) {
+//         if (p == nullptr && q == nullptr) return true;
+//         if (p == nullptr || q == nullptr) return false;
+
+//         if (p->val != q->val) return false;
+//         return helper(p->left, q->right) && helper(p->right, q->left);
+//     }
+
+//     bool isSymmetric(TreeNode* root) {
+//         if (root == nullptr) return true;
+//         return helper(root->left, root->right);
+//     }
+// };
+
+
+
+//OPTIMIZATION
 class Solution {
 public:
-    bool helper(TreeNode* p, TreeNode* q) {
-        if (p == nullptr && q == nullptr) return true;
-        if (p == nullptr || q == nullptr) return false;
+    int countNodes(TreeNode* root) {
+        if (!root) return 0;
 
-        if (p->val != q->val) return false;
-        return helper(p->left, q->right) && helper(p->right, q->left);
+        int leftHeight = getLeftHeight(root);
+        int rightHeight = getRightHeight(root);
+
+        if (leftHeight == rightHeight) {
+            // Perfect binary tree
+            return (1 << leftHeight) - 1;
+        }
+
+        // Otherwise, count recursively
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 
-    bool isSymmetric(TreeNode* root) {
-        if (root == nullptr) return true;
-        return helper(root->left, root->right);
+private:
+    int getLeftHeight(TreeNode* node) {
+        int height = 0;
+        while (node) {
+            height++;
+            node = node->left;
+        }
+        return height;
+    }
+
+    int getRightHeight(TreeNode* node) {
+        int height = 0;
+        while (node) {
+            height++;
+            node = node->right;
+        }
+        return height;
     }
 };
