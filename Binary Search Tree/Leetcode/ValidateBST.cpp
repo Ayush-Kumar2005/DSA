bed06de2f1035ac414b693e1b41c80c44dcc06bb
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-14 19:15:04 +0530

    Leetcode-98 : Another method

--- Binary Search Tree/Leetcode/ValidateBST.cpp
+++ Binary Search Tree/Leetcode/ValidateBST.cpp
@@ -1,4 +1,42 @@
-//--------------------------METHOD-1:Finding inorder and check whether it is sorted or not using an array---------------------------------
+//----------METHOD-1:for each node checking that root  val  is greater than max node value in in left subtree and smaller thanmin value of right subtree---------------------------------
+// class Solution {
+// public:
+//     int findMax(TreeNode* root) {
+//         while (root->right) {
+//             root = root->right;
+//         }
+//         return root->val;
+//     }
+
+//     int findMin(TreeNode* root) {
+//         while (root->left) {
+//             root = root->left;
+//         }
+//         return root->val;
+//     }
+
+//     bool isValidBST(TreeNode* root) {
+//         if (!root) return true;
+
+//         // Check left subtree max
+//         if (root->left) {
+//             int leftMax = findMax(root->left);
+//             if (leftMax >= root->val) return false;
+//         }
+
+//         // Check right subtree min
+//         if (root->right) {
+//             int rightMin = findMin(root->right);
+//             if (rightMin <= root->val) return false;
+//         }
+
+//         // Recursively check subtrees
+//         return isValidBST(root->left) && isValidBST(root->right);
+//     }
+// };
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+//--------------------------METHOD-2:Finding inorder and check whether it is sorted or not using an array---------------------------------
 // class Solution {
 // public:
 //     void inorder(TreeNode* root , vector<int>& v){
@@ -18,7 +56,7 @@
 // };
 //----------------------------------------------------------------------------------------------------------------------------------------------
 
-//--------------------------METHOD-2:Finding inorder and check whether it is sorted or not using a temp variable---------------------------------
+//--------------------------METHOD-3:Finding inorder and check whether it is sorted or not using a temp variable---------------------------------
 // class Solution {
 // public:
 //     bool inorder(TreeNode* root, long long &prev) {
