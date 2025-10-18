f757b92f4b3f8d6966c38239384112b21a7995c1
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-18 19:38:41 +0530

    2nd approach

--- String/Leetcode/ReverseVowels.cpp
+++ String/Leetcode/ReverseVowels.cpp
@@ -1,31 +1,54 @@
-class Solution {
-public:
-    int isVowel(char ch) {
-        string vowels = "aeiouAEIOU";
-        return vowels.find(ch) != string::npos;
-    }
+// class Solution {
+// public:
+//     int isVowel(char ch) {
+//         string vowels = "aeiouAEIOU";
+//         return vowels.find(ch) != string::npos;
+//     }
 
-    string reverseVowels(string s) {
-        int left = 0;
-        int right = s.length() - 1;
+//     string reverseVowels(string s) {
+//         int left = 0;
+//         int right = s.length() - 1;
 
-        while (left < right) {
-            if (!isVowel(s[left])) {
-                left++;
-                continue;
-            }
-            if (!isVowel(s[right])) {
-                right--;
-                continue;
-            }
+//         while (left < right) {
+//             if (!isVowel(s[left])) {
+//                 left++;
+//                 continue;
+//             }
+//             if (!isVowel(s[right])) {
+//                 right--;
+//                 continue;
+//             }
 
-            char t = s[left];
-            s[left] = s[right];
-            s[right] = t;
-            left++;
-            right--;
-        }
+//             char t = s[left];
+//             s[left] = s[right];
+//             s[right] = t;
+//             left++;
+//             right--;
+//         }
 
-        return s;
-    }
-};
+//         return s;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     bool isVowel(char c) {
+//         string v = "aeiouAEIOU";
+//         return v.find(c) != string::npos;
+//     }
+
+//     string reverseVowels(string s) {
+//         stack<char> st;
+//         for (char c : s) {
+//             if (isVowel(c)) st.push(c);
+//         }
+//         for (int i = 0; i < s.size(); i++) {
+//             if (isVowel(s[i])) {
+//                 s[i] = st.top();
+//                 st.pop();
+//             }
+//         }
+//         return s;
+//     }
+// };
