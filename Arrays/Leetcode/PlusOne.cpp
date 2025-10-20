8a5dc1c4f3ff4de99117eff7142c369a0f8481ed
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-20 20:02:52 +0530

    leetcode-66 - 3rd approach

--- Arrays/Leetcode/PlusOne.cpp
+++ Arrays/Leetcode/PlusOne.cpp
@@ -28,3 +28,25 @@
 //         return digits;
 //     }
 // };
+
+
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int>& digits) {
+//         string s = "";
+//         for(int d : digits) s += to_string(d);
+
+//         int carry = 1;
+//         for(int i = s.size() - 1; i >= 0; --i) {
+//             int sum = (s[i] - '0') + carry;
+//             s[i] = (sum % 10) + '0';
+//             carry = sum / 10;
+//             if(!carry) break;
+//         }
+//         if(carry) s.insert(s.begin(), '1');
+
+//         vector<int> result;
+//         for(char c : s) result.push_back(c - '0');
+//         return result;
+//     }
+// };
