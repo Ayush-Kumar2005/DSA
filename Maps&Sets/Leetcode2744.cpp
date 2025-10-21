9f6be62ac5477ecd468e19d8aa7a344b21687fff
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-21 20:13:17 +0530

    Leetcode - 2744 : Optimized approach

--- Maps&Sets/Leetcode2744.cpp
+++ Maps&Sets/Leetcode2744.cpp
@@ -39,3 +39,20 @@
 //         return count;
 //     }
 // };
+
+//3rd approach : optimized(single loop)
+// class Solution {
+// public:
+//     int maximumNumberOfStringPairs(vector<string>& words) {
+//         int n = words.size();
+//         int count=0;
+//         unordered_set<string> s;
+//         for(int i=0;i<n;i++){
+//             string rev=words[i];
+//             reverse(rev.begin(),rev.end());
+//             if(s.find(rev)!=s.end()) count++;
+//             else s.insert(words[i]);
+//         }
+//         return count;
+//     }
+// };
