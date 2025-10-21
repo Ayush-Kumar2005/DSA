df36bde61e1c0f70918678f673fd943f2d3e204c
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-21 20:28:17 +0530

    Valid ab=nagram : 3rd approach using single map

--- Maps&Sets/Leetcode242-ValidAnagram.cpp
+++ Maps&Sets/Leetcode242-ValidAnagram.cpp
@@ -32,3 +32,23 @@
 //         return true;
 //     }
 // };
+
+
+//Approach-3 : using single map
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if(s.length()!=t.length()) return false;
+//         unordered_map<char,int> map1;
+//         for(int i=0;i<s.length();i++) map1[s[i]]++;
+//         for(char ch : t){
+//             if(map1.find(ch)!=map1.end()){
+//                 map1[ch]--;
+//                 if(map1[ch]==0) map1.erase(ch);
+//             }
+//             else return false;
+//         }
+//         if(map1.size()>0) return false;
+//         return true;
+//     }
+// };
