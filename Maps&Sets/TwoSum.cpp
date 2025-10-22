6fa9b794899b68bf45202ec0d95aba779b4a7fc6
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-22 20:58:28 +0530

    two sum problem - hash-map approach

--- Maps&Sets/TwoSum.cpp
+++ Maps&Sets/TwoSum.cpp
@@ -47,3 +47,22 @@
 //         return {};
 //     }
 // };
+
+
+// ----------------------------------------------METHOD-3 : HASH-MAP APPROACH -----------------------------
+// #include <unordered_map>
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int, int> m;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             int required = target - nums[i];
+//             if (m.find(required) != m.end()) {
+//                 return {m[required], i};
+//             }
+//             m[nums[i]] = i;
+//         }
+//         return {};
+//     }
+// };
