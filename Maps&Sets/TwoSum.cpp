724ef5ba4014f766161dd3e4820d36c950cfcabb
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-22 20:58:00 +0530

    Two sum problem : two pointer approach

--- Maps&Sets/TwoSum.cpp
+++ Maps&Sets/TwoSum.cpp
@@ -14,3 +14,36 @@
 //         return {};
 //     }
 // };
+
+
+// ----------------------------METHOD-2: TWO POINTER APPROACH --------------------------------------------------------
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& arr, int target) {
+//         int n = arr.size();
+
+//         vector<pair<int,int>> nums;
+//         for (int i = 0; i < n; i++) {
+//             nums.push_back({arr[i], i});
+//         }
+
+//         sort(nums.begin(), nums.end());
+
+//         int low = 0, high = n - 1;
+
+//         while (low < high) {
+//             int sum = nums[low].first + nums[high].first;
+
+//             if (sum == target) {
+//                 return {nums[low].second, nums[high].second};
+//             }
+//             else if (sum < target) {
+//                 low++;
+//             }
+//             else {
+//                 high--;
+//             }
+//         }
+//         return {};
+//     }
+// };
