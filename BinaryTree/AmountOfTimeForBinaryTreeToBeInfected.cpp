59de238436a27be7b25714159fd74c08ffcf1103
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-24 20:51:56 +0530

    Amount of Time for Binary tree to be infected

---
+++ BinaryTree/AmountOfTimeForBinaryTreeToBeInfected.cpp
@@ -0,0 +1,61 @@
+// class Solution {
+// public:
+//     TreeNode* first = NULL;
+
+//     void find(TreeNode* root, int start) {
+//         if (!root) return;
+//         if (root->val == start) {
+//             first = root;
+//             return;
+//         }
+//         find(root->left, start);
+//         find(root->right, start);
+//     }
+
+//     void markParent(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& parent) {
+//         if (!root) return;
+//         if (root->left) parent[root->left] = root;
+//         if (root->right) parent[root->right] = root;
+//         markParent(root->left, parent);
+//         markParent(root->right, parent);
+//     }
+
+//     int amountOfTime(TreeNode* root, int start) {
+//         find(root, start);
+
+//         unordered_map<TreeNode*, TreeNode*> parent;
+//         markParent(root, parent);
+
+//         unordered_set<TreeNode*> isInfected;
+//         isInfected.insert(first);
+
+//         queue<pair<TreeNode*, int>> q;
+//         q.push({first, 0});
+
+//         int maxLevel = 0;
+
+//         while (!q.empty()) {
+//             auto [temp, level] = q.front();
+//             q.pop();
+//             maxLevel = max(maxLevel, level);
+
+//             if (temp->left && isInfected.find(temp->left) == isInfected.end()) {
+//                 q.push({temp->left, level + 1});
+//                 isInfected.insert(temp->left);
+//             }
+
+//             if (temp->right && isInfected.find(temp->right) == isInfected.end()) {
+//                 q.push({temp->right, level + 1});
+//                 isInfected.insert(temp->right);
+//             }
+
+//             if (parent.find(temp) != parent.end() &&
+//                 isInfected.find(parent[temp]) == isInfected.end()) {
+//                 q.push({parent[temp], level + 1});
+//                 isInfected.insert(parent[temp]);
+//             }
+//         }
+
+//         return maxLevel;
+//     }
+// };
