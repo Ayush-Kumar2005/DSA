bd4c9f9ad14e101f09fdeb1d429053f1f6a3e587
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-26 18:59:31 +0530

    Array partition approach 1

---
+++ Greedy/Array_Partition.cpp
@@ -0,0 +1,12 @@
+// -------------------------------------LEETCODE-561 : ARRAY PARTITION -------------------------------------------------------
+// APPROACH-1
+
+// class Solution {
+// public:
+//     int arrayPairSum(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         int maximizedSum=0;
+//         for(int i=0;i<nums.size();i+=2)  maximizedSum+=nums[i];
+//         return maximizedSum;
+//     }
+// };
