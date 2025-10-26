589906f429e6fd5bd226b64f9e156b9b828a5ddc
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-26 18:54:07 +0530

    Longest palindrome 2nd approach

--- Greedy/Longest_Palindrome.cpp
+++ Greedy/Longest_Palindrome.cpp
@@ -17,3 +17,31 @@
 //         return length;
 //     }
 // };
+
+
+//------------------------------------------------ GREEDY APPROACH ----------------------------------------------
+// 1. Use a set to track unpaired characters.
+// 2. Every time you see a character:
+// 3. If it's already in the set → remove it (we’ve formed a pair).
+//    Else → add it.
+// 4. At the end, if the set is not empty, we can put one unpaired character in the center.
+
+
+// class Solution {
+// public:
+//     int longestPalindrome(string s) {
+//         unordered_set<char> set;
+//         int length=0;
+//         for(char c : s){
+//             if(set.find(c)!=set.end()){     //character found inside the set => pair it
+//                 length+=2;
+//                 set.erase(c);
+//             }
+//             else{   // character not found for pairing
+//                 set.insert(c);
+//             }
+//         }
+//         if(set.size()>0) length++;
+//         return length;
+//     }
+// };
