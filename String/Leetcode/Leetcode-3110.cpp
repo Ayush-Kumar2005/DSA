2c343c66bf8bd07aadbfe767dc2b40dbfd4f97ac
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-29 22:54:10 +0530

    approach-2

--- String/Leetcode/Leetcode-3110.cpp
+++ String/Leetcode/Leetcode-3110.cpp
@@ -8,3 +8,19 @@
 //         return ans;
 //     }
 // };
+
+
+// class Solution {
+// public:
+//     int scoreOfString(string s) {
+//         int n = s.size();
+//         if (n < 2) return 0;  // though constraints guarantee n ≥ 2
+//         int ans = 0;
+//         for (int i = 1; i < n; ++i) {
+//             int prev = s[i-1];
+//             int curr = s[i];
+//             ans += abs(curr - prev);
+//         }
+//         return ans;
+//     }
+// };
