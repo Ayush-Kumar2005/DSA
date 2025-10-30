d71946d33728e623ce5d645c934efd503998482d
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-10-30 22:01:25 +0530

    approach-2

--- Binary Search/Leetcode/Leetcode1385.cpp
+++ Binary Search/Leetcode/Leetcode1385.cpp
@@ -14,3 +14,26 @@
 //     }
 //     return count;
 // }
+
+
+// --------------------------------APPROACH-2 : SORT+BINARY SEARCH ------------------------------
+// class Solution {
+// public:
+//     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+//         sort(arr2.begin(), arr2.end());
+//         int count = 0;
+        
+//         for (int x : arr1) {
+//             auto it = lower_bound(arr2.begin(), arr2.end(), x);
+//             bool valid = true;
+            
+//             if (it != arr2.end() && abs(*it - x) <= d) valid = false;
+//             if (it != arr2.begin() && abs(*(it - 1) - x) <= d) valid = false;
+            
+//             if (valid) count++;
+//         }
+//         return count;
+//     }
+
+
+// };
